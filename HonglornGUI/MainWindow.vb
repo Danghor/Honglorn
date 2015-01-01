9fca7feda3396ca481a0c284b43b71dc38c3147d
Danghor <danghor+github.com@gmail.com>
2015-01-01 05:49:40 +0100

    add SetDisciplinesDialog-Form, move auxiliary functions to Tools-Class

--- HonglornGUI/MainWindow.vb
+++ HonglornGUI/MainWindow.vb
@@ -2,6 +2,10 @@
 
 Public Class MainWindow
   Private Const SCALE_FACTOR As Single = 0.8
+  Friend _oApp As Honglorn
+  Private _oSetDisciplinesDialog As SetDisciplinesDialog
+
+#Region "Properties"
 
   Private ReadOnly Property CurrentYear As Integer
     Get
@@ -17,6 +21,19 @@ Public Class MainWindow
     End Get
   End Property
 
+  Private ReadOnly Property SetDisciplinesDialog As SetDisciplinesDialog
+    Get
+      If _oSetDisciplinesDialog IsNot Nothing Then
+        SetDisciplinesDialog = _oSetDisciplinesDialog
+      Else
+        _oSetDisciplinesDialog = New SetDisciplinesDialog()
+        SetDisciplinesDialog = _oSetDisciplinesDialog
+      End If
+    End Get
+  End Property
+
+#End Region
+
   'todo: replace this by login form
   Private Sub SETCREDENTIALS()
     Dim oFile As New System.IO.StreamReader("C:\Git\Honglorn\CREDEN~1.TXT")
@@ -24,10 +41,8 @@ Public Class MainWindow
     Dim sUser As String = oFile.ReadLine()
     Dim sPassword As String = oFile.ReadLine()
     Dim sDatabase As String = oFile.ReadLine()
-    App = New Honglorn(sServer, sUser, sPassword)
+    _oApp = New Honglorn(sServer, sUser, sPassword)
   End Sub
-
-  Private App As Honglorn
 
   Private Sub MainWindow_Load(sender As Object, e As EventArgs) Handles MyBase.Load
     SETCREDENTIALS()
@@ -35,10 +50,10 @@ Public Class MainWindow
     Me.Height = CInt(Math.Round(Screen.PrimaryScreen.Bounds.Height * SCALE_FACTOR))
     Me.Width = CInt(Math.Round(Screen.PrimaryScreen.Bounds.Width * SCALE_FACTOR))
 
-    Me.Location = New Point(CInt(Math.Round((Screen.PrimaryScreen.Bounds.Width - Me.Width) / 2)), CInt(Math.Round((Screen.PrimaryScreen.Bounds.Height - Me.Height) / 2)))
+    Tools.Center(Me)
 
     'pre-select a year
-    Dim aiValidYears As Integer() = App.GetValidYears()
+    Dim aiValidYears As Integer() = _oApp.GetValidYears()
     If aiValidYears.Count <> 0 Then
       SelectEditYearComboBox.DataSource = aiValidYears
       SelectEditYearComboBox.SelectedIndex = 0
@@ -54,16 +69,16 @@ Public Class MainWindow
   End Sub
 
   Private Sub SelectEditYearComboBox_DropDown(sender As Object, e As EventArgs) Handles SelectEditYearComboBox.DropDown
-    SelectEditYearComboBox.DataSource = App.GetValidYears()
+    SelectEditYearComboBox.DataSource = _oApp.GetValidYears()
   End Sub
 
   Private Sub SelectEditCourseComboBox_DropDown(sender As Object, e As EventArgs) Handles SelectEditCourseComboBox.DropDown
     If CurrentYear <> -1 Then
-      Dim asNewCourseNames As String() = App.GetValidCourseNames(CurrentYear)
+      Dim asNewCourseNames As String() = _oApp.GetValidCourseNames(CurrentYear)
       Dim asOldCourseNames As String() = CType(SelectEditCourseComboBox.DataSource, String())
-      
-      If Not IsEqual(asNewCourseNames, asOldCourseNames) Then
-        SelectEditCourseComboBox.DataSource = App.GetValidCourseNames(CurrentYear)
+
+      If Not Tools.IsEqual(asNewCourseNames, asOldCourseNames) Then
+        SelectEditCourseComboBox.DataSource = _oApp.GetValidCourseNames(CurrentYear)
       End If
     Else
       'todo: display tooltip "please set a year" or so
@@ -74,12 +89,12 @@ Public Class MainWindow
     'todo: confirmation dialog, saving changes from old datasource etc
     'todo: change tab order so tab at the end of the line skips to next "value" field instead of "Surname"
     If EditDataGridView.DataSource IsNot Nothing Then
-      App.SaveChanges(CType(EditDataGridView.DataSource, DataTable))
+      _oApp.SaveChanges(CType(EditDataGridView.DataSource, DataTable))
     End If
 
     EditDataGridView.Visible = False
 
-    EditDataGridView.DataSource = App.GetCompetitionEditDataTable(sCourseName, iYear)
+    EditDataGridView.DataSource = _oApp.GetCompetitionEditDataTable(sCourseName, iYear)
 
     EditDataGridView.Columns("PKey").Visible = False
     EditDataGridView.Columns("Surname").ReadOnly = True
@@ -100,28 +115,10 @@ Public Class MainWindow
   Private Sub EditDataGridView_CellEndEdit(sender As Object, e As DataGridViewCellEventArgs) Handles EditDataGridView.CellEndEdit
     'todo: verify content
 
-    App.SaveChanges(CType(EditDataGridView.DataSource, DataTable))
+    _oApp.SaveChanges(CType(EditDataGridView.DataSource, DataTable))
   End Sub
 
-  ''' <summary>
-  ''' Compares two String arrays. Returns true if their content is identical and false otherwise.
-  ''' </summary>
-  ''' <param name="asFirst"></param>
-  ''' <param name="asSecond"></param>
-  ''' <returns></returns>
-  ''' <remarks>Uses Exit Function.</remarks>
-  Private Function IsEqual(asFirst As String(), asSecond As String()) As Boolean
-    IsEqual = True
-
-    If asFirst IsNot Nothing AndAlso asSecond IsNot Nothing AndAlso asFirst.Count = asSecond.Count Then
-      For i As Integer = 0 To asFirst.Count - 1
-        If asFirst(i) <> asSecond(i) Then
-          IsEqual = False
-          Exit Function 'MURICA!
-        End If
-      Next
-    Else
-      IsEqual = False
-    End If
-  End Function
+  Private Sub SetDisciplinesToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SetDisciplinesToolStripMenuItem.Click
+    SetDisciplinesDialog.ShowDialog(Me)
+  End Sub
 End Class
---
+++ HonglornGUI/Tools.vb
@@ -0,0 +1,34 @@
+﻿Friend Class Tools
+
+  ''' <summary>
+  ''' Positions the given control in the center of the screen.
+  ''' </summary>
+  ''' <param name="oControl">The control to be centered.</param>
+  ''' <remarks></remarks>
+  Public Shared Sub Center(oControl As Control)
+    oControl.Location = New Point(CInt(Math.Round((Screen.PrimaryScreen.Bounds.Width - oControl.Width) / 2)), CInt(Math.Round((Screen.PrimaryScreen.Bounds.Height - oControl.Height) / 2)))
+  End Sub
+
+  ''' <summary>
+  ''' Compares two String arrays. Returns true if their content is identical and false otherwise.
+  ''' </summary>
+  ''' <param name="asFirst"></param>
+  ''' <param name="asSecond"></param>
+  ''' <returns></returns>
+  ''' <remarks>Uses Exit For.</remarks>
+  Public Shared Function IsEqual(asFirst As String(), asSecond As String()) As Boolean
+    IsEqual = True
+
+    If asFirst IsNot Nothing AndAlso asSecond IsNot Nothing AndAlso asFirst.Count = asSecond.Count Then
+      For i As Integer = 0 To asFirst.Count - 1
+        If asFirst(i) <> asSecond(i) Then
+          IsEqual = False
+          Exit For
+        End If
+      Next
+    Else
+      IsEqual = False
+    End If
+  End Function
+
+End Class
