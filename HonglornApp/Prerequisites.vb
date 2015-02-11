1e2002f55ca73cd68a2acc2cddd164b16ce406d5
Danghor <danghor+github.com@gmail.com>
2015-02-11 19:42:15 +0100

    add validation function for years

--- HonglornApp/Prerequisites.vb
+++ HonglornApp/Prerequisites.vb
@@ -1,4 +1,6 @@
-﻿Public Module Prerequisites
+﻿Imports System.Text.RegularExpressions
+
+Public Module Prerequisites
 
   Public Const DATABASE_NAME As String = "bjs"
 
@@ -20,5 +22,19 @@
     Male
     Female
   End Enum
+
+  ''' <summary>
+  ''' Returns true if the given input year is a value between (including) 1900 and 2099
+  ''' </summary>
+  ''' <param name="iYear">The year to be validated.</param>
+  ''' <returns>True if the given year is a valid year.</returns>
+  ''' <remarks></remarks>
+  Function IsValidYear(iYear As Integer) As Boolean
+    If Regex.IsMatch(CStr(iYear), "(19|20)[0-9]{2}") Then
+      IsValidYear = True
+    Else
+      IsValidYear = False
+    End If
+  End Function
 
 End Module
