3899a6fab2e09b6e07215bb0351925ebf91b00b2
Danghor <danghor+github.com@gmail.com>
2016-02-15 20:39:37 +0100

    Create WPF Project

---
+++ HonglornAUT/TraditionalCalculatorTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HonglornAUT {
+  [TestClass]
+  public class TraditionalCalculatorTest {
+
+    [TestMethod]
+    public void TestMethod1() {
+    }
+  }
+}
