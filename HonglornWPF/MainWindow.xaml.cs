baf440ab84749f52429764256d250267e81e09a3
Danghor <danghor+github.com@gmail.com>
2017-02-27 00:09:04 +0100

    Create fresh new WPF project

--- HonglornWPF/MainWindow.xaml.cs
+++ HonglornWPF/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,13 +12,12 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using MahApps.Metro.Controls;
 
 namespace HonglornWPF {
   /// <summary>
   /// Interaction logic for MainWindow.xaml
   /// </summary>
-  partial class MainWindow {
+  public partial class MainWindow : Window {
     public MainWindow() {
       InitializeComponent();
     }
