0fd8ab325064c6180f94108747c39860a14f8915
Danghor <danghor+github.com@gmail.com>
2017-02-28 23:54:51 +0100

    Add EditPerformanceView and use it as the first tab of the application

---
+++ HonglornWPF/ViewModels/EditPerformanceViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HonglornWPF.ViewModels {
+  class EditPerformanceViewModel {
+    public event Action CoursesLoaded;
+    public event Action YearsLoaded;
+
+    public ObservableCollection<string> Courses { get; set; }
+    public ObservableCollection<short> Years { get; set; }
+
+    public EditPerformanceViewModel() {
+      Courses = new ObservableCollection<string>();
+      Years = new ObservableCollection<short>();
+    }
+
+    public void LoadCourseNames(short year) {
+      Courses.Clear();
+
+      ICollection<string> courseNames = HonglornBL.Honglorn.ValidCourseNames(year);
+
+      foreach (string courseName in courseNames) {
+        Courses.Add(courseName);
+      }
+
+      CoursesLoaded?.Invoke();
+    }
+
+    public void LoadYears() {
+      Years.Clear();
+
+      ICollection<short> years = HonglornBL.Honglorn.YearsWithStudentData();
+
+      foreach (short year in years) {
+        Years.Add(year);
+      }
+
+      YearsLoaded?.Invoke();
+    }
+  }
+}
