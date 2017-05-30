90ecb52c83a53395ba66dc89bbef0c905b784af8
Danghor <danghor+github.com@gmail.com>
2017-05-30 11:40:35 +0200

    Simplify code

--- HonglornWPF/CompetitionValueConverter.cs
+++ HonglornWPF/CompetitionValueConverter.cs
@@ -8,20 +8,6 @@ namespace HonglornWPF
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => value;
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            object returnValue;
-
-            if (string.IsNullOrWhiteSpace(value as string))
-            {
-                returnValue = null;
-            }
-            else
-            {
-                returnValue = value;
-            }
-
-            return returnValue;
-        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => string.IsNullOrWhiteSpace(value as string) ? null : value;
     }
 }
