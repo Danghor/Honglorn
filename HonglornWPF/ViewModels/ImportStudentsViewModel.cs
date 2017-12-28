3a1dffade675ac19f634362798e735f6e4aa264d
Danghor <danghor+github.com@gmail.com>
2017-12-28 23:46:10 +0100

    Add ImportStudentsView and auxiliary classes

---
+++ HonglornWPF/ViewModels/ImportStudentsViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HonglornWPF.ViewModels
+{
+    class ImportStudentsViewModel : ViewModel
+    {
+
+        short year;
+
+        public short Year
+        {
+            get { return year; }
+            set
+            {
+                year = value;
+                OnPropertyChanged(nameof(Year));
+            }
+        }
+
+        public ImportStudentsViewModel()
+        {
+            Year = (short)(DateTime.Now.Year);
+        }
+    }
+}
