d9ae5cf2401679404ae9b72b88d7eadec9cf5799
Danghor <danghor+github.com@gmail.com>
2018-01-02 18:43:44 +0100

    Implement file dialog to set path in ImportStudents module

--- HonglornWPF/ViewModels/ImportStudentsViewModel.cs
+++ HonglornWPF/ViewModels/ImportStudentsViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Input;
 
 namespace HonglornWPF.ViewModels
 {
@@ -9,6 +10,8 @@ namespace HonglornWPF.ViewModels
     {
 
         short year;
+        string path;
+        ICommand openFileDialogCommand;
 
         public short Year
         {
@@ -20,9 +23,40 @@ namespace HonglornWPF.ViewModels
             }
         }
 
+        public string Path
+        {
+            get { return path; }
+            set
+            {
+                path = value;
+                OnPropertyChanged(nameof(Path));
+            }
+        }
+
+        public ICommand OpenFileDialogCommand
+        {
+            get { return openFileDialogCommand; }
+            set
+            {
+                openFileDialogCommand = value;
+                OnPropertyChanged(nameof(OpenFileDialogCommand));
+            }
+        }
+
         public ImportStudentsViewModel()
         {
             Year = (short)(DateTime.Now.Year);
+            OpenFileDialogCommand = new RelayCommand(OpenFileDialog);
+        }
+
+        private void OpenFileDialog()
+        {
+            Microsoft.Win32.OpenFileDialog dialog = new Microsoft.Win32.OpenFileDialog();
+
+            if (dialog.ShowDialog() == true)
+            {
+                Path = dialog.FileName;
+            }
         }
     }
 }
---
+++ HonglornWPF/ViewModels/RelayCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace HonglornWPF.ViewModels
+{
+    class RelayCommand : ICommand
+    {
+        Action Action { get; }
+
+        public event EventHandler CanExecuteChanged;
+
+        internal RelayCommand(Action action)
+        {
+            Action = action;
+        }
+
+        public bool CanExecute(object parameter) => true;
+
+        public void Execute(object parameter)
+        {
+            Action?.Invoke();
+        }
+    }
+}
