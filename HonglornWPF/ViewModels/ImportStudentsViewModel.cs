4b33cdb384bc1c4f0c6d63ee8397f32ec26c5230
Danghor <danghor+github.com@gmail.com>
2018-01-02 19:41:55 +0100

    Write stub to import student data

--- HonglornWPF/ViewModels/ImportStudentsViewModel.cs
+++ HonglornWPF/ViewModels/ImportStudentsViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HonglornBL.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -9,8 +10,10 @@ namespace HonglornWPF.ViewModels
 {
     class ImportStudentsViewModel : ViewModel
     {
+        readonly BackgroundWorker importWorker;
         short year;
         string path;
+        private object filePathTextBox;
 
         public short Year
         {
@@ -33,14 +36,21 @@ namespace HonglornWPF.ViewModels
         }
 
         public ICommand OpenFileDialogCommand { get; }
+        public ICommand ImportStudentsAsynCommand { get; }
 
         public ImportStudentsViewModel()
         {
             Year = (short)(DateTime.Now.Year);
             OpenFileDialogCommand = new RelayCommand(OpenFileDialog);
+            ImportStudentsAsynCommand = new RelayCommand(ImportStudentsAsync);
+
+            importWorker = new BackgroundWorker();
+            importWorker.WorkerReportsProgress = true;
+            importWorker.DoWork += ImportStudents;
+            importWorker.ProgressChanged += ProgressChanged;
         }
 
-        private void OpenFileDialog()
+        void OpenFileDialog()
         {
             Microsoft.Win32.OpenFileDialog dialog = new Microsoft.Win32.OpenFileDialog();
 
@@ -48,6 +58,22 @@ namespace HonglornWPF.ViewModels
             {
                 Path = dialog.FileName;
             }
+        }
+
+        void ImportStudentsAsync()
+        {
+            importWorker.RunWorkerAsync();
+        }
+
+        void ImportStudents(object sender, DoWorkEventArgs e)
+        {
+            HonglornBL.Honglorn.ImportStudentCourseExcelSheet(Path, Year, importWorker);
+        }
+
+        void ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            IProgressInformer informer = e.UserState as IProgressInformer;
+            Console.WriteLine(informer.StatusMessage);
         }
     }
 }
