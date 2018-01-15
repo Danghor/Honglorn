4528f0b8b49d60b2b72ab83551cd964fc8b3a05d
Danghor <danghor+github.com@gmail.com>
2018-01-15 01:14:26 +0100

    Load disciplines in the comboboxes for the assign disciplines module

--- HonglornWPF/ViewModels/AssignDisciplinesViewModel.cs
+++ HonglornWPF/ViewModels/AssignDisciplinesViewModel.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using HonglornBL.Models.Entities;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
 using static HonglornBL.Prerequisites;
+using static HonglornBL.Honglorn;
 
 namespace HonglornWPF.ViewModels
 {
@@ -12,15 +12,27 @@ namespace HonglornWPF.ViewModels
         IDictionary<Game?, RadioButtonGame> GameTypeMap = new Dictionary<Game?, RadioButtonGame>
         {
             { HonglornBL.Prerequisites.Game.Traditional, RadioButtonGame.Traditional },
-            { HonglornBL.Prerequisites.Game.Competition, RadioButtonGame.Competition },
+            { HonglornBL.Prerequisites.Game.Competition, RadioButtonGame.Competition }
         };
 
         public ObservableCollection<string> Classes { get; set; } = new ObservableCollection<string>();
         public ObservableCollection<short> Years { get; set; } = new ObservableCollection<short>();
+        public ObservableCollection<Discipline> MaleSprintDisciplines { get; set; } = new ObservableCollection<Discipline>();
+        public ObservableCollection<Discipline> MaleJumpDisciplines { get; set; } = new ObservableCollection<Discipline>();
+        public ObservableCollection<Discipline> MaleThrowDisciplines { get; set; } = new ObservableCollection<Discipline>();
+        public ObservableCollection<Discipline> MaleMiddleDistanceDisciplines { get; set; } = new ObservableCollection<Discipline>();
+        public ObservableCollection<Discipline> FemaleSprintDisciplines { get; set; } = new ObservableCollection<Discipline>();
+        public ObservableCollection<Discipline> FemaleJumpDisciplines { get; set; } = new ObservableCollection<Discipline>();
+        public ObservableCollection<Discipline> FemaleThrowDisciplines { get; set; } = new ObservableCollection<Discipline>();
+        public ObservableCollection<Discipline> FemaleMiddleDistanceDisciplines { get; set; } = new ObservableCollection<Discipline>();
 
         string currentClass;
         short currentYear;
         RadioButtonGame game;
+        Discipline currentMaleSprintDiscipline;
+        Discipline currentMaleJumpDiscipline;
+        Discipline currentMaleThrowDiscipline;
+        Discipline currentMaleMiddleDistanceDiscipline;
 
         public short CurrentYear
         {
@@ -48,8 +60,8 @@ namespace HonglornWPF.ViewModels
                 currentClass = value;
                 OnPropertyChanged(nameof(CurrentClass));
 
-                Game? gameFromDb = HonglornBL.Honglorn.GetGameType(CurrentClass, CurrentYear);
-                if(gameFromDb == null)
+                Game? gameFromDb = GetGameType(CurrentClass, CurrentYear);
+                if (gameFromDb == null)
                 {
                     Game = RadioButtonGame.Unknown;
                 }
@@ -67,12 +79,128 @@ namespace HonglornWPF.ViewModels
             {
                 game = value;
                 OnPropertyChanged(nameof(Game));
+
+                if (value == RadioButtonGame.Traditional)
+                {
+                    LoadAllTraditionalDisciplines();
+                }
+                else if (value == RadioButtonGame.Competition)
+                {
+                    LoadAllCompetitionDisciplines();
+                }
+
             }
         }
 
-        void LoadClassNames() => ClearAndFill(Classes, HonglornBL.Honglorn.ValidClassNames(CurrentYear));
+        public Discipline CurrentMaleSprintDiscipline
+        {
+            get { return currentMaleSprintDiscipline; }
+            set
+            {
+                currentMaleSprintDiscipline = value;
+                OnPropertyChanged(nameof(CurrentMaleSprintDiscipline));
+            }
+        }
 
-        void LoadYears() => ClearAndFill(Years, HonglornBL.Honglorn.YearsWithStudentData());
+        public Discipline CurrentMaleJumpDiscipline
+        {
+            get { return currentMaleSprintDiscipline; }
+            set
+            {
+                currentMaleSprintDiscipline = value;
+                OnPropertyChanged(nameof(CurrentMaleJumpDiscipline));
+            }
+        }
+
+        public Discipline CurrentMaleThrowDiscipline
+        {
+            get { return currentMaleSprintDiscipline; }
+            set
+            {
+                currentMaleSprintDiscipline = value;
+                OnPropertyChanged(nameof(CurrentMaleThrowDiscipline));
+            }
+        }
+
+        public Discipline CurrentMaleMiddleDistanceDiscipline
+        {
+            get { return currentMaleSprintDiscipline; }
+            set
+            {
+                currentMaleSprintDiscipline = value;
+                OnPropertyChanged(nameof(CurrentMaleMiddleDistanceDiscipline));
+            }
+        }
+
+        public Discipline CurrentFemaleSprintDiscipline
+        {
+            get { return currentMaleSprintDiscipline; }
+            set
+            {
+                currentMaleSprintDiscipline = value;
+                OnPropertyChanged(nameof(CurrentFemaleSprintDiscipline));
+            }
+        }
+
+        public Discipline CurrentFemaleJumpDiscipline
+        {
+            get { return currentMaleSprintDiscipline; }
+            set
+            {
+                currentMaleSprintDiscipline = value;
+                OnPropertyChanged(nameof(CurrentFemaleJumpDiscipline));
+            }
+        }
+
+        public Discipline CurrentFemaleThrowDiscipline
+        {
+            get { return currentMaleSprintDiscipline; }
+            set
+            {
+                currentMaleSprintDiscipline = value;
+                OnPropertyChanged(nameof(CurrentFemaleThrowDiscipline));
+            }
+        }
+
+        public Discipline CurrentFemaleMiddleDistanceDiscipline
+        {
+            get { return currentMaleSprintDiscipline; }
+            set
+            {
+                currentMaleSprintDiscipline = value;
+                OnPropertyChanged(nameof(CurrentFemaleMiddleDistanceDiscipline));
+            }
+        }
+
+        void LoadClassNames() => ClearAndFill(Classes, ValidClassNames(CurrentYear));
+
+        void LoadYears() => ClearAndFill(Years, YearsWithStudentData());
+
+        void LoadAllCompetitionDisciplines()
+        {
+            ClearAndFill(MaleSprintDisciplines, FilteredCompetitionDisciplines(DisciplineType.Sprint));
+            ClearAndFill(MaleJumpDisciplines, FilteredCompetitionDisciplines(DisciplineType.Jump));
+            ClearAndFill(MaleThrowDisciplines, FilteredCompetitionDisciplines(DisciplineType.Throw));
+            ClearAndFill(MaleMiddleDistanceDisciplines, FilteredCompetitionDisciplines(DisciplineType.MiddleDistance));
+
+            ClearAndFill(FemaleSprintDisciplines, FilteredCompetitionDisciplines(DisciplineType.Sprint));
+            ClearAndFill(FemaleJumpDisciplines, FilteredCompetitionDisciplines(DisciplineType.Jump));
+            ClearAndFill(FemaleThrowDisciplines, FilteredCompetitionDisciplines(DisciplineType.Throw));
+            ClearAndFill(FemaleMiddleDistanceDisciplines, FilteredCompetitionDisciplines(DisciplineType.MiddleDistance));
+        }
+
+        void LoadAllTraditionalDisciplines()
+        {
+            ClearAndFill(MaleSprintDisciplines, FilteredTraditionalDisciplines(DisciplineType.Sprint, Sex.Male));
+            ClearAndFill(MaleJumpDisciplines, FilteredTraditionalDisciplines(DisciplineType.Jump, Sex.Male));
+            ClearAndFill(MaleThrowDisciplines, FilteredTraditionalDisciplines(DisciplineType.Throw, Sex.Male));
+            ClearAndFill(MaleMiddleDistanceDisciplines, FilteredTraditionalDisciplines(DisciplineType.MiddleDistance, Sex.Male));
+
+            ClearAndFill(FemaleSprintDisciplines, FilteredTraditionalDisciplines(DisciplineType.Sprint, Sex.Female));
+            ClearAndFill(FemaleJumpDisciplines, FilteredTraditionalDisciplines(DisciplineType.Jump, Sex.Female));
+            ClearAndFill(FemaleThrowDisciplines, FilteredTraditionalDisciplines(DisciplineType.Throw, Sex.Female));
+            ClearAndFill(FemaleMiddleDistanceDisciplines, FilteredTraditionalDisciplines(DisciplineType.MiddleDistance, Sex.Female));
+        }
 
         public AssignDisciplinesViewModel()
         {
