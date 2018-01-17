f962112ec2114581b70903647003475ef0cf2c0e
Danghor <danghor+github.com@gmail.com>
2018-01-17 17:43:21 +0100

    Remove unnecessary usings

--- HonglornBL/CompetitionDisciplineContainer.cs
+++ HonglornBL/CompetitionDisciplineContainer.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using HonglornBL.Models.Entities;
+﻿using HonglornBL.Models.Entities;
 
 namespace HonglornBL
 {
