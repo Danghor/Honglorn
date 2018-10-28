1ab9f546f7bb07dc5afc2577b9e9db46940caaeb
Danghor <danghor+github.com@gmail.com>
2018-10-28 21:34:58 +0100

    Move enums to own file; use Game enum from BL instead of own one in AssignDisciplinesViewModel

---
+++ HonglornBL/Enums.cs
@@ -0,0 +1,35 @@
+﻿namespace HonglornBL.Enums
+{
+    public enum Certificate
+    {
+        Honorary = 0,
+        Victory = 1,
+        Participation = 2
+    }
+
+    public enum DisciplineType
+    {
+        Sprint = 0,
+        Jump = 1,
+        Throw = 2,
+        MiddleDistance = 3
+    }
+
+    public enum Game
+    {
+        Traditional = 0,
+        Competition = 1
+    }
+
+    public enum Measurement
+    {
+        Manual = 0,
+        Electronic = 1
+    }
+
+    public enum Sex
+    {
+        Male = 0,
+        Female = 1
+    }
+}
