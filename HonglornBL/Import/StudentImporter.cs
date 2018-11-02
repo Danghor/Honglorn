8c5575996060fd2b9c8a37b51865af2de1995781
Danghor <danghor+github.com@gmail.com>
2018-11-02 20:44:00 +0100

    Introduce abstract class StudentImporter

---
+++ HonglornBL/Import/StudentImporter.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace HonglornBL.Import
+{
+    abstract class StudentImporter : IStudentImporter
+    {
+        public abstract ICollection<ImportedStudentRecord> ReadStudentsFromFile(string filePath);
+    }
+}
