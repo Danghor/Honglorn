090ded6ff0afd458411746532eff1329cb615393
Unknown <danghor+github.com@gmail.com>
2018-11-03 16:41:02 +0100

    Fix IDisciplineCollection interface for local db

--- HonglornBL/Interfaces/IDisciplineCollection.cs
+++ HonglornBL/Interfaces/IDisciplineCollection.cs
@@ -4,15 +4,15 @@ namespace HonglornBL.Interfaces
 {
     public interface IDisciplineCollection
     {
-        Guid MaleSprintPKey { get; }
-        Guid MaleJumpPKey { get; }
-        Guid MaleThrowPKey { get; }
-        Guid MaleMiddleDistancePKey { get; }
+        Guid? MaleSprintPKey { get; }
+        Guid? MaleJumpPKey { get; }
+        Guid? MaleThrowPKey { get; }
+        Guid? MaleMiddleDistancePKey { get; }
 
-        Guid FemaleSprintPKey { get; }
-        Guid FemaleJumpPKey { get; }
-        Guid FemaleThrowPKey { get; }
-        Guid FemaleMiddleDistancePKey { get; }
+        Guid? FemaleSprintPKey { get; }
+        Guid? FemaleJumpPKey { get; }
+        Guid? FemaleThrowPKey { get; }
+        Guid? FemaleMiddleDistancePKey { get; }
 
     }
 }
