1fca337e563d5f79b182be531a1376960b076f08
Unknown <danghor+github.com@gmail.com>
2018-11-03 18:39:53 +0100

    Add sample AUT using localdb

---
+++ HonglornAUT/TestDbInitializer.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Entity;
+
+namespace HonglornAUT
+{
+    public partial class TraditionalCalculatorTest
+    {
+        class TestDbInitializer : DropCreateDatabaseAlways<TestDb> { }
+    }
+}
--- HonglornAUT/TraditionalCalculatorTest.cs
+++ HonglornAUT/TraditionalCalculatorTest.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using System.Linq;
+using HonglornBL.Models.Entities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace HonglornAUT {
-  [TestClass]
-  public class TraditionalCalculatorTest {
+namespace HonglornAUT
+{
+    [TestClass]
+    public partial class TraditionalCalculatorTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (var db = new TestDb())
+            {
+                db.Student.Add(new Student
+                {
+                    Forename = "Cave",
+                    Surname = "Johnson"
+                });
 
-    [TestMethod]
-    public void TestMethod1() {
+                db.SaveChanges();
+            }
+
+            using (var db = new TestDb())
+            {
+                Assert.AreEqual(1, db.Student.Count());
+            }
+        }
     }
-  }
 }
