40733966fbf43b66e1eb036b4de092a442a8ffcc
Unknown <danghor+github.com@gmail.com>
2018-11-03 18:39:53 +0100

    Add sample AUT using localdb

---
+++ HonglornAUT/TestDb.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.Entity;
+using HonglornBL.Models.Entities;
+
+namespace HonglornAUT
+{
+    public partial class TraditionalCalculatorTest
+    {
+        sealed class TestDb : DbContext
+        {
+            public DbSet<Competition> Competition { get; set; }
+            public DbSet<CompetitionDiscipline> CompetitionDiscipline { get; set; }
+            public DbSet<CompetitionReportMeta> CompetitionReportMeta { get; set; }
+            public DbSet<DisciplineCollection> DisciplineCollection { get; set; }
+            public DbSet<Student> Student { get; set; }
+            public DbSet<StudentCourseRel> StudentCourseRel { get; set; }
+            public DbSet<TraditionalDiscipline> TraditionalDiscipline { get; set; }
+            public DbSet<TraditionalReportMeta> TraditionalReportMeta { get; set; }
+
+            public TestDb() : base("name=test")
+            {
+                Database.SetInitializer(new TestDbInitializer());
+            }
+        }
+    }
+}
