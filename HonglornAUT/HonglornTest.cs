318d4b37a2a2f9dc7b4fdae45cc9eba9e61e7f48
Unknown <danghor+github.com@gmail.com>
2018-11-05 20:17:33 +0100

    Implement on full stack test for tradtitional game type

--- HonglornAUT/HonglornTest.cs
+++ HonglornAUT/HonglornTest.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Data.Common;
 using System.Linq;
+using System.Transactions;
 using HonglornBL;
 using HonglornBL.Enums;
 using HonglornBL.Import;
+using HonglornBL.Interfaces;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace HonglornAUT
@@ -41,16 +43,65 @@ namespace HonglornAUT
         {
             var sut = new Honglorn(CreateConnection());
 
-            sut.ImportSingleStudent("Dave", "Pennington", Sex.Male, 1998, "08C", 2018);
-            sut.ImportSingleStudent("Hannah", "Smith", Sex.Female, 1997, "08C", 2018);
+            sut.ImportSingleStudent("Dave", "Pennington", Sex.Male, 2008, "08C", 2018);
+            sut.ImportSingleStudent("Hannah", "Smith", Sex.Female, 2007, "08C", 2018);
 
+            Guid sprintPKey = (from d in sut.FilteredTraditionalDisciplines(DisciplineType.Sprint, Sex.Male)
+                              where d.ToString() == "Sprint 100 m (Manual)"
+                              select d.PKey).Single();
 
-            IEnumerable<IStudentPerformance> studentPerformance = sut.StudentPerformances("08B", 2018);
+            Guid jumpPKey = (from d in sut.FilteredTraditionalDisciplines(DisciplineType.Jump, Sex.Male)
+                            where d.ToString() == "Weitsprung"
+                            select d.PKey).Single();
 
-            Assert.Inconclusive();
+            Guid throwPKey = (from d in sut.FilteredTraditionalDisciplines(DisciplineType.Throw, Sex.Male)
+                             where d.ToString() == "Kugelstoß"
+                             select d.PKey).Single();
+
+            Guid middleDistancePKey = (from d in sut.FilteredTraditionalDisciplines(DisciplineType.MiddleDistance, Sex.Male)
+                                      where d.ToString() == "Lauf 1000 m"
+                                      select d.PKey).Single();
+
+            Guid sprintFPKey = (from d in sut.FilteredTraditionalDisciplines(DisciplineType.Sprint, Sex.Female)
+                               where d.ToString() == "Sprint 100 m (Manual)"
+                               select d.PKey).Single();
+
+            Guid jumpFPKey = (from d in sut.FilteredTraditionalDisciplines(DisciplineType.Jump, Sex.Female)
+                where d.ToString() == "Hochsprung"
+                select d.PKey).Single();
+
+            Guid throwFPKey = (from d in sut.FilteredTraditionalDisciplines(DisciplineType.Throw, Sex.Female)
+                              where d.ToString() == "200-g-Ballwurf"
+                              select d.PKey).Single();
+
+            Guid middleDistanceFPKey = (from d in sut.FilteredTraditionalDisciplines(DisciplineType.MiddleDistance, Sex.Female)
+                                       where d.ToString() == "Lauf 800 m"
+                                       select d.PKey).Single();
+
+            sut.CreateOrUpdateDisciplineCollection("8", 2018, sprintPKey, jumpPKey, throwPKey, middleDistancePKey, sprintFPKey, jumpFPKey, throwFPKey, middleDistanceFPKey);
+
+            IEnumerable<IStudentPerformance> performances = sut.StudentPerformances("08C", 2018).ToList();
+
+            Guid davePKey = performances.Single(p => p.Forename == "Dave").StudentPKey;
+            Guid hannahPKey = performances.Single(p => p.Forename == "Hannah").StudentPKey;
+
+            sut.UpdateSingleStudentCompetition(davePKey, 2018, 13.9f, 4.37f, 8.2f, 294);
+            sut.UpdateSingleStudentCompetition(hannahPKey, 2018, 14.9f, 1.44f, 32.5f, 234);
+
+            IEnumerable<IResult> results = sut.GetResultsAsync("08C", 2018).Result.ToList();
+
+            IResult daveResult = results.Single(r => r.Forename == "Dave");
+            IResult hannahResult = results.Single(r => r.Forename == "Hannah");
+
+            Assert.AreEqual(1428, daveResult.Score);
+            Assert.AreEqual(Certificate.Honorary, daveResult.Certificate);
+
+            Assert.AreEqual(1492, hannahResult.Score);
+            Assert.AreEqual(Certificate.Honorary, hannahResult.Certificate);
         }
 
         [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
         public void ImportStudentsFromFile_EmptyFileName_RaisesException()
         {
             var sut = new Honglorn(CreateConnection());
@@ -61,7 +112,7 @@ namespace HonglornAUT
             }
             catch (AggregateException e)
             {
-                Assert.IsInstanceOfType(e.InnerExceptions.Single(), typeof(ArgumentException));
+                throw e.InnerExceptions.Single();
             }
         }
     }
