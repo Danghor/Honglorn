311035afe7eb61a6ed37f70e155cfae529c6ff85
Unknown <danghor+github.com@gmail.com>
2018-11-05 17:45:33 +0100

    Improve tests

--- HonglornAUT/HonglornTest.cs
+++ HonglornAUT/HonglornTest.cs
@@ -1,43 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using HonglornBL;
 using HonglornBL.Enums;
 using HonglornBL.Import;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Data.Common;
 
 namespace HonglornAUT
 {
     [TestClass]
     public class HonglornTest
     {
-        [TestMethod]
-        public void ImportSingleStudent_Regular_StudentSuccessfullyAdded()
+        DbConnection CreateConnection()
         {
-            DbConnection connection = Effort.DbConnectionFactory.CreateTransient();
-
-            var sut = new Honglorn(connection);
-
-            sut.ImportSingleStudent("Cave", "Johnson", Sex.Male, 1928, "08B", 2018);
-
-            IEnumerable<IStudentPerformance> studentPerformance = sut.StudentPerformances("08B", 2018);
-
-            Assert.AreEqual(1, studentPerformance.Count());
+            return Effort.DbConnectionFactory.CreateTransient();
         }
 
         [TestMethod]
-        public void ImportSingleStudent_Regular_StudentSuccessfullyAdded2()
+        public void ImportSingleStudent_Regular_StudentSuccessfullyAdded()
         {
-            DbConnection connection = Effort.DbConnectionFactory.CreateTransient();
+            string forename = "Cave";
+            string surname = "Johnson";
+            string courseName = "08B";
+            short year = 2018;
 
-            var sut = new Honglorn(connection);
+            var sut = new Honglorn(CreateConnection());
 
-            sut.ImportSingleStudent("Cave", "Johnson", Sex.Male, 1928, "08C", 2018);
+            sut.ImportSingleStudent(forename, surname, Sex.Male, 1928, courseName, year);
+
+            IStudentPerformance studentPerformance = sut.StudentPerformances(courseName, year).Single();
+
+            Assert.IsNull(forename, studentPerformance.Forename);
+            Assert.IsNull(surname, studentPerformance.Surname);
+            Assert.IsNull(studentPerformance.Sprint);
+            Assert.IsNull(studentPerformance.Jump);
+            Assert.IsNull(studentPerformance.Throw);
+            Assert.IsNull(studentPerformance.MiddleDistance);
+        }
+
+        [TestMethod]
+        public void GetResults_TraditionalCompetition_CorrectScoresAndCertificatesCalculated()
+        {
+            var sut = new Honglorn(CreateConnection());
+
+            sut.ImportSingleStudent("Dave", "Pennington", Sex.Male, 1998, "08C", 2018);
+            sut.ImportSingleStudent("Hannah", "Smith", Sex.Female, 1997, "08C", 2018);
+
 
             IEnumerable<IStudentPerformance> studentPerformance = sut.StudentPerformances("08B", 2018);
 
-            Assert.AreEqual(0, studentPerformance.Count());
+            Assert.Inconclusive();
         }
 
         [TestMethod]
@@ -45,7 +58,7 @@ namespace HonglornAUT
         {
             DbConnection connection = Effort.DbConnectionFactory.CreateTransient();
 
-            var sut = new Honglorn(connection);
+            var sut = new Honglorn(CreateConnection());
 
             try
             {
