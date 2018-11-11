f04cb24d21c36cf1766ea6035fbb8851594231b7
Danghor <danghor+github.com@gmail.com>
2018-11-11 18:09:56 +0100

    Remove unnecessary usings

--- HonglornBL/Models/Entities/Student.cs
+++ HonglornBL/Models/Entities/Student.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
 using HonglornBL.Enums;
 
 namespace HonglornBL.Models.Entities
