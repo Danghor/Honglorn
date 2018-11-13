32c5f0b39e83b0cb42e1fcc4049a9af39d859414
Unknown <danghor+github.com@gmail.com>
2018-11-13 16:41:47 +0100

    Return the score for the individual disciplines in the results

--- HonglornBL/Interfaces/IResult.cs
+++ HonglornBL/Interfaces/IResult.cs
@@ -6,6 +6,12 @@ namespace HonglornBL
     {
         string Forename { get; }
         string Surname { get; }
+
+        ushort SprintScore { get; }
+        ushort JumpScore { get; }
+        ushort ThrowScore { get; }
+        ushort MiddleDistanceScore { get; }
+
         ushort Score { get; }
         Certificate Certificate { get; }
     }
--- HonglornBL/Result.cs
+++ HonglornBL/Result.cs
@@ -6,13 +6,23 @@ namespace HonglornBL
     {
         public string Forename { get; }
         public string Surname { get; }
+
+        public ushort SprintScore { get; }
+        public ushort JumpScore { get; }
+        public ushort ThrowScore { get; }
+        public ushort MiddleDistanceScore { get; }
+
         public ushort Score { get; }
         public Certificate Certificate { get; }
 
-        internal Result(string forename, string surname, ushort score, Certificate certificate)
+        internal Result(string forename, string surname, ushort sprintScore, ushort jumpScore, ushort throwScore, ushort middleDistanceScore, ushort score, Certificate certificate)
         {
             Forename = forename;
             Surname = surname;
+            SprintScore = sprintScore;
+            JumpScore = jumpScore;
+            ThrowScore = throwScore;
+            MiddleDistanceScore = middleDistanceScore;
             Score = score;
             Certificate = certificate;
         }
