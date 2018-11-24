ce20d6fc52eb5315315a4f1e8c0a2219880160c2
Unknown <danghor+github.com@gmail.com>
2018-11-24 22:53:16 +0100

    Remove unnecessary usings

--- HonglornBL/Models/Framework/HonglornDbInitializer.cs
+++ HonglornBL/Models/Framework/HonglornDbInitializer.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Data.Entity;
 using System.IO;
-using System.Runtime.Serialization;
 using System.Xml.Serialization;
 using HonglornBL.Models.Entities;
 using HonglornBL.Properties;
