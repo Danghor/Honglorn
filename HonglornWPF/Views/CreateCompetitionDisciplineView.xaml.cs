3b0cb824f8b79165e7eb32b5b5f98bcd1e783d6d
Unknown <danghor+github.com@gmail.com>
2018-12-06 16:53:14 +0100

    Fix compilation errors and remove redundant code

--- HonglornWPF/Views/CreateCompetitionDisciplineView.xaml.cs
+++ HonglornWPF/Views/CreateCompetitionDisciplineView.xaml.cs
@@ -1,23 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace HonglornWPF.Views
+﻿namespace HonglornWPF.Views
 {
     /// <summary>
     /// Interaction logic for CreateCompetitionDisciplineView.xaml
     /// </summary>
-    public partial class CreateCompetitionDisciplineView : UserControl
+    partial class CreateCompetitionDisciplineView
     {
         public CreateCompetitionDisciplineView()
         {
