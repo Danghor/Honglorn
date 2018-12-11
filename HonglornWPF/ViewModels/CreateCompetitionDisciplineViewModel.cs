d3020ffeb15b4e57187a4f9506d6ddebfeb756c3
Unknown <danghor+github.com@gmail.com>
2018-12-11 07:19:16 +0100

    Add CreateCompetitionsDisciplineViewModel

---
+++ HonglornWPF/ViewModels/CreateCompetitionDisciplineViewModel.cs
@@ -0,0 +1,35 @@
+﻿using HonglornBL.Enums;
+
+namespace HonglornWPF.ViewModels
+{
+    class CreateCompetitionDisciplineViewModel : ViewModel
+    {
+        string name;
+        public string Name
+        {
+            get { return name; }
+            set { OnPropertyChanged(out name, value); }
+        }
+
+        DisciplineType type;
+        DisciplineType Type
+        {
+            get { return type; }
+            set { OnPropertyChanged(out type, value); }
+        }
+
+        string unit;
+        public string Unit
+        {
+            get { return unit; }
+            set { OnPropertyChanged(out unit, value); }
+        }
+
+        bool lowIsBetter;
+        public bool LowIsBetter
+        {
+            get { return lowIsBetter; }
+            set { OnPropertyChanged(out lowIsBetter, value); }
+        }
+    }
+}
