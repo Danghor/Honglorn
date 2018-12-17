9cd02ff0e4e9f180c57870cc3cc9c34e443add30
Unknown <danghor+github.com@gmail.com>
2018-12-17 17:56:54 +0100

    Disable update and delete buttons when no discipline is selected

---
+++ HonglornWPF/Converter/ObjectToBoolConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace HonglornWPF.Converter
+{
+    [ValueConversion(typeof(object), typeof(bool))]
+    class ObjectToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
