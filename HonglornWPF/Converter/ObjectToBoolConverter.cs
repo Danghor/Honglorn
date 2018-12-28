c87cd106f502dc9e770f1a6d40eba2a3c6a1c9ee
Unknown <danghor+github.com@gmail.com>
2018-12-28 23:44:14 +0100

    Only enable save button in AssignDisciplinesView when all disciplines are set

--- HonglornWPF/Converter/ObjectToBoolConverter.cs
+++ HonglornWPF/Converter/ObjectToBoolConverter.cs
@@ -1,18 +1,19 @@
 ﻿using System;
 using System.Globalization;
+using System.Linq;
 using System.Windows.Data;
 
 namespace HonglornWPF.Converter
 {
-    [ValueConversion(typeof(object), typeof(bool))]
-    class ObjectToBoolConverter : IValueConverter
+    [ValueConversion(typeof(object[]), typeof(bool))]
+    class ObjectsNotNullToBoolConverter : IMultiValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
-            return value != null;
+            return values.All(i => i != null);
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
         }
