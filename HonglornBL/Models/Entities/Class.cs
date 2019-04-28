93ca2ca66e80157e675f5db68c9b8b1e6c29c6fb
Danghor <danghor+github.com@gmail.com>
2019-04-28 23:36:38 +0200

    Minor cleanup

--- HonglornBL/Models/Entities/Class.cs
+++ HonglornBL/Models/Entities/Class.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HonglornBL.Models.Entities
@@ -13,7 +12,7 @@ namespace HonglornBL.Models.Entities
 
         public Class Clone()
         {
-            return new Class()
+            return new Class
             {
                 Name = Name
             };
