8742f58a44a7a1425f4bbece45a47960266b6254
Danghor <danghor+github.com@gmail.com>
2019-05-06 21:33:31 +0200

    Tweak datamodel

--- HonglornBL/Games/Traditional/TrackAndField/TraditionalTrackAndFieldDiscipline.cs
+++ HonglornBL/Games/Traditional/TrackAndField/TraditionalTrackAndFieldDiscipline.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using HonglornBL.Enums;
 using HonglornBL.Models.Entities;
+using System.ComponentModel.DataAnnotations;
 
 namespace HonglornBL
 {
     public abstract class TraditionalTrackAndFieldDiscipline : Discipline
     {
+        [Required]
+        public Sex Sex { get; set; }
+
         internal int CalculateScore(Student student, double? value)
         {
             return value == null ? 0 : CalculateNonNullScore(student, value.Value);
