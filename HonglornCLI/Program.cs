bde107312ad4be92c4ea395b5023b2fa88235199
Danghor <danghor+github.com@gmail.com>
2019-05-07 22:39:21 +0200

    Add CLI project for testing

---
+++ HonglornCLI/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using HonglornBL;
+
+namespace HonglornCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var honglorn = new Honglorn(ConfigurationManager.ConnectionStrings["HonglornDb"]);
+
+            var games = honglorn.GetGames();
+        }
+    }
+}
