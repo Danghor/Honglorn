4bc97956ed4e194fa5b65dd47049bc722a6517f0
Danghor <danghor+github.com@gmail.com>
2019-05-07 22:39:46 +0200

    Fix crashed on context access

--- HonglornBL/Models/Entities/Class.cs
+++ HonglornBL/Models/Entities/Class.cs
@@ -14,13 +14,5 @@ namespace HonglornBL.Models.Entities
         [Index(IsUnique = true)]
         [StringLength(25)]
         public string Name { get; set; }
-
-        public Class Clone()
-        {
-            return new Class
-            {
-                Name = Name
-            };
-        }
     }
 }
