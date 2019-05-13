9ea2f45cfb5ceff9543f3d6f92cf8b31b6f847d0
Danghor <danghor+github.com@gmail.com>
2019-05-13 17:28:41 +0200

    Add proper name for test

---
+++ HonglornAUT/TraditionalTrackAndFieldTest.cs
@@ -0,0 +1,42 @@
+﻿using HonglornBL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Data.Common;
+using System.Linq;
+
+namespace HonglornAUT
+{
+    [TestClass]
+    [DeploymentItem("EntityFramework.SqlServer.dll")]
+    public class TraditionalTrackAndFieldTest
+    {
+        static DbConnection CreateConnection() => Effort.DbConnectionFactory.CreateTransient();
+
+        public TestContext TestContext { get; set; }
+
+        const string GameName = "Leichtathletik Wettkampf";
+        DateTime GameDate = DateTime.Parse("1/1/2019");
+
+        TraditionalTrackAndFieldGameManager CreateGameManager()
+        {
+            var sut = new Honglorn(CreateConnection());
+
+            sut.CreateTraditionalTrackAndFieldGame(GameName, GameDate);
+
+            return sut.GetGames().TraditionalTrackAndFieldGames.Single();
+        }
+
+        [TestMethod]
+        public void CreateGameShouldSaveNameAndDate()
+        {
+            var sut = new Honglorn(CreateConnection());
+
+            sut.CreateTraditionalTrackAndFieldGame(GameName, GameDate);
+
+            TraditionalTrackAndFieldGameManager game = sut.GetGames().TraditionalTrackAndFieldGames.Single();
+
+            Assert.AreEqual(GameName, game.GameName);
+            Assert.AreEqual(GameDate, game.GameDate);
+        }
+    }
+}
