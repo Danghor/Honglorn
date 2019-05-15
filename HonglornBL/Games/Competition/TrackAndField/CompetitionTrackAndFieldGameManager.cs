f70d5d7f07d07a58b70ea9690a96a99cbd417c29
Danghor <danghor+github.com@gmail.com>
2019-05-15 16:40:45 +0200

    Add editing methods to CompetitionTrackAndFieldGameManager

--- HonglornBL/Games/Competition/TrackAndField/CompetitionTrackAndFieldGameManager.cs
+++ HonglornBL/Games/Competition/TrackAndField/CompetitionTrackAndFieldGameManager.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using HonglornBL.Games.Competition.TrackAndField;
 using HonglornBL.Models.Framework;
 
 namespace HonglornBL
 {
-    public class CompetitionTrackAndFieldGameManager
+    public class CompetitionTrackAndFieldGameManager : IGameManager<CompetitionTrackAndFieldResult>
     {
         internal HonglornDbFactory ContextFactory { get; set; }
 
@@ -13,6 +15,35 @@ namespace HonglornBL
         {
             GamePKey = gamePKey;
             ContextFactory = contextFactory;
+        }
+
+        CompetitionTrackAndFieldGame Game(HonglornDb db)
+        {
+            CompetitionTrackAndFieldGame game = db.CompetitionTrackAndFieldGame.Find(GamePKey);
+
+            if (game == null)
+            {
+                throw new GameNotFoundException($"No game with key {GamePKey} found.");
+            }
+
+            return game;
+        }
+
+        public ICollection<CompetitionTrackAndFieldResult> CalculateResults()
+        {
+            throw new NotImplementedException();
+
+            using (HonglornDb db = ContextFactory.CreateContext())
+            {
+                var results = new List<CompetitionTrackAndFieldResult>();
+
+                foreach (var group in Game(db).EvaluationGroups)
+                {
+                    
+                }
+
+                return results;
+            }
         }
     }
 }
--- HonglornBL/Games/Competition/TrackAndField/CompetitionTrackAndFieldResult.cs
+++ HonglornBL/Games/Competition/TrackAndField/CompetitionTrackAndFieldResult.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HonglornBL.Enums;
+using HonglornBL.Models.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,5 +9,8 @@ namespace HonglornBL.Games.Competition.TrackAndField
 {
     public class CompetitionTrackAndFieldResult
     {
+        int TotalScore { get; }
+        CertificateType CertificateType { get; }
+        Student Student { get; }
     }
 }
