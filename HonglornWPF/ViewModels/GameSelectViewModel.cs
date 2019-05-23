60926d4ae22484d88208d7ee518673d1718b1cf1
Danghor <danghor+github.com@gmail.com>
2019-05-23 22:38:18 +0200

    Create UI stub

---
+++ HonglornWPF/ViewModels/GameSelectViewModel.cs
@@ -0,0 +1,16 @@
+﻿using HonglornBL;
+
+namespace HonglornWPF.ViewModels
+{
+    class GameSelectViewModel : ViewModel
+    {
+        GameCollection gameCollection;
+
+        public GameCollection GameCollection
+        {
+            get => gameCollection;
+            set => OnPropertyChanged(out gameCollection, value);
+        }
+
+    }
+}
---
+++ HonglornWPF/Views/GameSelectView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace HonglornWPF.Views
+{
+    /// <summary>
+    /// Interaction logic for GameSelectView.xaml
+    /// </summary>
+    public partial class GameSelectView : UserControl
+    {
+        public GameSelectView()
+        {
+            InitializeComponent();
+        }
+    }
+}
