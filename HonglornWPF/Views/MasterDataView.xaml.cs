09fd1dcb410397df50fdd1cbe2e9c4a725ee2ea5
Danghor <danghor+github.com@gmail.com>
2019-05-24 07:17:47 +0200

    Design basic layout

---
+++ HonglornWPF/Views/MasterDataView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HonglornWPF.Views
+{
+    /// <summary>
+    /// Interaction logic for MasterDataView.xaml
+    /// </summary>
+    public partial class MasterDataView : UserControl
+    {
+        public MasterDataView()
+        {
+            InitializeComponent();
+        }
+    }
+}
