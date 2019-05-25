ef1d37b7cc3c797581dfd2257c63040e1a080c75
Danghor <danghor+github.com@gmail.com>
2019-05-25 19:27:00 +0200

    Add ClassListView

---
+++ HonglornWPF/ViewModels/ClassListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using HonglornBL;
+
+namespace HonglornWPF.ViewModels
+{
+    class ClassListViewModel : ViewModel
+    {
+        public ObservableCollection<ClassManager> ClassManagers { get; } = new ObservableCollection<ClassManager>();
+    }
+}
