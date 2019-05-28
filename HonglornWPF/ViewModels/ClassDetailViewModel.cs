b49f1a81a6622989811ffa67f925e3c1af50a5be
Danghor <danghor+github.com@gmail.com>
2019-05-28 17:04:40 +0200

    Further detail view experiments

--- HonglornWPF/ViewModels/ClassDetailViewModel.cs
+++ HonglornWPF/ViewModels/ClassDetailViewModel.cs
@@ -2,10 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Input;
 
 namespace HonglornWPF.ViewModels
 {
     class ClassDetailViewModel : ViewModel
     {
+        public enum Mode { Add, Edit }
+
+        public ICommand UpdateCommand { get; }
+
+        public ClassDetailViewModel()
+        {
+            UpdateCommand = new RelayCommand(Update);
+        }
+
+        void Update()
+        {
+
+        }
     }
 }
