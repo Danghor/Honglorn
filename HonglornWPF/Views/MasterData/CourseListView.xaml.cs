7328d65905a5fd3620945480f0678c74ede9ec1b
Danghor <danghor+github.com@gmail.com>
2019-06-03 20:34:29 +0200

    Remove redundant code

--- HonglornWPF/Views/MasterData/CourseListView.xaml.cs
+++ HonglornWPF/Views/MasterData/CourseListView.xaml.cs
@@ -1,11 +1,9 @@
-﻿using System.Windows.Controls;
-
-namespace HonglornWPF.Views.MasterData
+﻿namespace HonglornWPF.Views.MasterData
 {
     /// <summary>
     /// Interaction logic for CourseListView.xaml
     /// </summary>
-    public partial class CourseListView : UserControl
+    public partial class CourseListView
     {
         public CourseListView()
         {
