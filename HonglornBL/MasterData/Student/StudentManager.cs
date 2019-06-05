f1bd38f9b7add8fc1a6455b12a70b9f978d8c80b
Danghor <danghor+github.com@gmail.com>
2019-06-05 16:14:59 +0200

    Fully implement StudentManager

--- HonglornBL/MasterData/Student/StudentManager.cs
+++ HonglornBL/MasterData/Student/StudentManager.cs
@@ -1,4 +1,5 @@
-﻿using HonglornBL.Exceptions;
+﻿using HonglornBL.Enums;
+using HonglornBL.Exceptions;
 using HonglornBL.Models.Entities;
 using HonglornBL.Models.Framework;
 using System;
@@ -6,9 +7,41 @@ using System.Data.Entity;
 
 namespace HonglornBL
 {
-    public class StudentManager : EntityManager<Student>
+    public class StudentManager : EntityManager<Student>, IStudentModel, IEntityManager<IStudentModel>
     {
         internal StudentManager(Guid pKey, HonglornDbFactory contextFactory) : base(pKey, contextFactory) { }
+
+        public string Surname
+        {
+            get => GetValue(s => s.Surname);
+            set => SetValue((student, surname) => student.Surname = surname, value);
+        }
+
+        public string Forename
+        {
+            get => GetValue(s => s.Forename);
+            set => SetValue((student, forename) => student.Forename = forename, value);
+        }
+
+        public Sex Sex
+        {
+            get => GetValue(s => s.Sex);
+            set => SetValue((student, sex) => student.Sex = sex, value);
+        }
+
+        public DateTime DateOfBirth
+        {
+            get => GetValue(s => s.DateOfBirth);
+            set => SetValue((student, dateOfBirth) => student.DateOfBirth = dateOfBirth, value.Date);
+        }
+
+        public void Update(IStudentModel model)
+        {
+            Surname = model.Surname;
+            Forename = model.Forename;
+            Sex = model.Sex;
+            DateOfBirth = model.DateOfBirth;
+        }
 
         protected override Exception CreateNotFoundException(string message) => new StudentNotFoundException(message);
 
