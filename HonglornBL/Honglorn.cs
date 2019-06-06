014d6ec97201acbf656e5227e4e0b2adb163d2c2
Danghor <danghor+github.com@gmail.com>
2019-06-06 19:37:22 +0200

    Add HandicapService

--- HonglornBL/Honglorn.cs
+++ HonglornBL/Honglorn.cs
@@ -1,4 +1,5 @@
 using HonglornBL.MasterData.Course;
+using HonglornBL.MasterData.Handicap;
 using HonglornBL.MasterData.StudentCourse;
 using HonglornBL.Models.Framework;
 using System;
@@ -50,6 +51,8 @@ namespace HonglornBL
         public CourseService CourseService() => new CourseService(ContextFactory);
 
         public StudentService StudentService() => new StudentService(ContextFactory);
+
+        public HandicapService HandicapService() => new HandicapService(ContextFactory);
 
         public StudentCourseService StudentCourseService() => new StudentCourseService(ContextFactory);
     }
---
+++ HonglornBL/MasterData/Handicap/HandicapService.cs
@@ -0,0 +1,28 @@
+﻿using HonglornBL.Models.Framework;
+using System;
+
+namespace HonglornBL.MasterData.Handicap
+{
+    public sealed class HandicapService : EntityService<HandicapManager, Models.Entities.Handicap, IHandicapModel>
+    {
+        internal HandicapService(HonglornDbFactory contextFactory) : base(contextFactory) { }
+
+        protected override Models.Entities.Handicap CreateEntity(IHandicapModel model)
+        {
+            return new Models.Entities.Handicap
+            {
+                Name = model.Name
+            };
+        }
+
+        protected override HandicapManager CreateManager(Guid pKey, HonglornDbFactory contextFactory)
+        {
+            return new HandicapManager(pKey, contextFactory);
+        }
+
+        protected override System.Data.Entity.IDbSet<Models.Entities.Handicap> GetDbSet(HonglornDb context)
+        {
+            return context.Handicap;
+        }
+    }
+}
