7ff53ff1360d7e0cb509497bc2b64e93e28214d6
Danghor <danghor+github.com@gmail.com>
2019-06-17 20:28:04 +0200

    Make game selection show list view properly

---
+++ HonglornWPF/ViewModels/CompetitionTrackAndFieldGameWrapper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using HonglornBL;
+
+namespace HonglornWPF.ViewModels
+{
+    class CompetitionTrackAndFieldGameWrapper : GameWrapper<CompetitionTrackAndFieldGameManager>
+    {
+        public CompetitionTrackAndFieldGameWrapper(CompetitionTrackAndFieldGameManager manager) : base(manager) { }
+
+        public override string Name => manager.Name;
+
+        public override DateTime Date => manager.Date;
+    }
+}
--- HonglornWPF/ViewModels/GameSelectViewModel.cs
+++ HonglornWPF/ViewModels/GameSelectViewModel.cs
@@ -1,22 +1,20 @@
 ﻿using HonglornBL;
-using HonglornBL.Game;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
 
 namespace HonglornWPF.ViewModels
 {
     class GameSelectViewModel : ViewModel
     {
-        public readonly ObservableCollection<IGameWrapper> Wrappers = new ObservableCollection<IGameWrapper>();
+        public ObservableCollection<IGameWrapper> Wrappers { get; } = new ObservableCollection<IGameWrapper>();
 
         public GameSelectViewModel()
         {
-            GameCollection GameCollection = Honglorn.GetGames();
+            GameCollection gameCollection = Honglorn.GetGames();
 
-            AddManagers(GameCollection.TraditionalTrackAndFieldGames, m => new TraditionalTrackAndFieldGameWrapper(m));
-            AddManagers(GameCollection.CompetitionTrackAndFieldGames, m => new CompetitionTrackAndFieldGameWrapper(m));
+            AddManagers(gameCollection.TraditionalTrackAndFieldGames, m => new TraditionalTrackAndFieldGameWrapper(m));
+            AddManagers(gameCollection.CompetitionTrackAndFieldGames, m => new CompetitionTrackAndFieldGameWrapper(m));
         }
 
         void AddManagers<TGameManager, TWrapper>(IEnumerable<TGameManager> gameManagers, Func<TGameManager, TWrapper> createWrapper)
