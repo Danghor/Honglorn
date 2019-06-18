825e2d8fa5d6bcba88e856d85f6b70d39987f447
Danghor <danghor+github.com@gmail.com>
2019-06-18 07:20:00 +0200

    Add unique index to GamePerformance

--- HonglornBL/Models/Entities/GamePerformance.cs
+++ HonglornBL/Models/Entities/GamePerformance.cs
@@ -5,19 +5,23 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HonglornBL.Models.Entities
 {
-    public abstract class GamePerformance<TMeasuringPoint, TDiscipline, TGamePerformance> : IEntity where TMeasuringPoint : MeasuringPoint<TDiscipline> where TDiscipline : Discipline
+    public abstract class GamePerformance<TMeasuringPoint, TDiscipline, TGamePerformance> : IEntity
+        where TMeasuringPoint : MeasuringPoint<TDiscipline>
+        where TDiscipline : Discipline
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public Guid PKey { get; set; } = Guid.NewGuid();
 
         [Required]
+        [Index("UniqueStudentGame", 1, IsUnique = true)]
         public Guid StudentPKey { get; set; }
 
         [ForeignKey(nameof(StudentPKey))]
         public virtual Student Student { get; set; }
 
         [Required]
+        [Index("UniqueStudentGame", 2, IsUnique = true)]
         public Guid GamePKey { get; set; }
 
         [ForeignKey(nameof(GamePKey))]
