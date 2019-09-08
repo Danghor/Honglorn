0746973b6c66984c586ad15662e786313a6853e0
Danghor <danghor+github.com@gmail.com>
2019-09-08 21:50:35 +0200

    Add buttons to add and remove StudentCourses

--- HonglornWPF/ViewModels/StudentDetailViewModel.cs
+++ HonglornWPF/ViewModels/StudentDetailViewModel.cs
@@ -1,11 +1,18 @@
 ﻿using HonglornBL.MasterData.Student;
 using HonglornBL.Models.Entities;
 using System;
+using System.Windows.Input;
 
 namespace HonglornWPF.ViewModels
 {
     class StudentDetailViewModel : NGDetailViewModel<StudentService, Student>
     {
-        public StudentDetailViewModel(StudentService service, Guid entityKey) : base(service, entityKey) { }
+        public ICommand AddStudentCourseCommand { get; }
+        public ICommand RemoveStudentCourseCommand { get; }
+
+        public StudentDetailViewModel(StudentService service, Guid entityKey) : base(service, entityKey)
+        {
+
+        }
     }
 }
