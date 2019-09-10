46546a2a58d2861a8b6697f3ac2c8a3b4a604469
Danghor <danghor+github.com@gmail.com>
2019-09-10 18:01:11 +0200

    WIP: Implement StudentCourse lookup

---
+++ HonglornWPF/ViewModels/LookupViewModel.cs
@@ -0,0 +1,12 @@
+﻿using HonglornBL.MasterData;
+using HonglornBL.Models.Entities;
+
+namespace HonglornWPF.ViewModels
+{
+    class LookupViewModel<TService, TEntity> : ServiceViewModel<TService, TEntity>
+        where TService : NGService<TEntity>
+        where TEntity : Entity
+    {
+        internal LookupViewModel(TService service) : base(service) { }
+    }
+}
--- HonglornWPF/ViewModels/StudentDetailViewModel.cs
+++ HonglornWPF/ViewModels/StudentDetailViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using HonglornBL.MasterData.Student;
+using HonglornBL.MasterData.StudentCourse;
 using HonglornBL.Models.Entities;
 using System;
 using System.Windows.Input;
@@ -10,9 +11,34 @@ namespace HonglornWPF.ViewModels
         public ICommand AddStudentCourseCommand { get; }
         public ICommand RemoveStudentCourseCommand { get; }
 
+        LookupViewModel<StudentCourseService, StudentCourse> studentCourseLookupViewModel;
+
+        public LookupViewModel<StudentCourseService, StudentCourse> StudentCourseLookupViewModel
+        {
+            get => studentCourseLookupViewModel;
+            set
+            {
+                studentCourseLookupViewModel?.Dispose();
+                OnPropertyChanged(out studentCourseLookupViewModel, value);
+            }
+        }
+
         public StudentDetailViewModel(StudentService service, Guid entityKey) : base(service, entityKey)
         {
-
+            AddStudentCourseCommand = new RelayCommand(AddStudentCourse);
+            RemoveStudentCourseCommand = new RelayCommand(RemoveStudentCourse);
         }
+
+        void AddStudentCourse()
+        {
+            throw new NotImplementedException();
+        }
+
+        void RemoveStudentCourse()
+        {
+            throw new NotImplementedException();
+        }
+
+        //TODO: Implement dispose pattern for the LookupViewModel
     }
 }
